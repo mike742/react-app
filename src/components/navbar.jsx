04b08b84c6aafa911c6dc9612c9db4e1db0f79dd
Mike Zorin <mykhailo.zorin@gmail.com>
2022-11-01 16:29:27 -0500

    init2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 function Navbar(props) {
     console.log('this.props', props);
+    const url = "";
     return (  
         <nav className='navbar navbar-light bg-light'>
             <div className='container-fluid'>
-                <a href="#" className='navbar-brand'>
+                <a href={url} className='navbar-brand'>
                     Total items: {props.totalCounters }
                 </a>
 
